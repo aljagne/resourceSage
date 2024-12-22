d26289a72d83a38fd0edd694675f2c05c11d2354
Alieu Jagne <ajagne01@gmail.com>
2024-12-22 23:47:21 +0100

    Revert "Feature/sprint1"

--- src/utils/api_clients/cloud_provider_api.ts
+++
@@ -1,3 +0,0 @@
-export interface CloudProviderAPI {
-  getResources(): Promise<any[]>;
-}
--- src/utils/api_clients/saas_api.ts
+++
@@ -1,4 +0,0 @@
-// src/utils/api_clients/saas_api.ts
-export interface SaasAPI {
-    getSubscriptionDetails(): Promise<any>;
-}
